7a12f51e65391af26032f23a31af87c4ede91618
Jordan Lim <limjordanm@gmail.com>
2025-09-16 22:19:33 -0700

    Fixed GAS URL

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // config.js
-export const GAS_BASE_URL = "https://script.google.com/macros/s/AKfycbzGWNXdX_gYyejT8mOmbFTmHGPkQxtoYhi5Tb1lfawDGVrrP1hgiHUHwrPFN62j7MKA/exec";
+export const GAS_BASE_URL = "https://script.google.com/macros/s/AKfycbzEJ0uxGTjRxmncSi_wlhjJXObCsG1cEZCFLex8ZYxSJThUVHV_xqtVeMG9RXX8qD0/exec";
