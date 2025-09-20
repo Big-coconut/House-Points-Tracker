9df00dcba467292abb03541190c7cdd4158662f1
Jordan Lim <limjordanm@gmail.com>
2025-09-20 12:29:39 -0700

    New GAS deployment, flipped returned json order

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // config.js
-export const GAS_BASE_URL = "https://script.google.com/macros/s/AKfycbzEJ0uxGTjRxmncSi_wlhjJXObCsG1cEZCFLex8ZYxSJThUVHV_xqtVeMG9RXX8qD0/exec";
+export const GAS_BASE_URL = "https://script.google.com/macros/s/AKfycbyAt9DM87EHb_b3PH_kmj8HOvMa1YkCyIEDYMsne4tqc0xehUp3OhH09Nn0pm-a6NEI/exec";
